f7a7c7c2fe535f867c7553f61fda161964cc863c
Thalita Rodrigues <112733274+thalitaasuzy@users.noreply.github.com>
2024-09-19 16:37:03 -0300

    Update potentiometer.cpp

--- potentiometer.cpp
+++ potentiometer.cpp
@@ -1,21 +1,21 @@
-const int ledPin = 3; //pino led
-const int potPin = A0; //pino potenciometro
+const int ledPin = 3; // Pino dos LEDs
+const int potPin = A0; //Pino do potenciometro
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT); //configura o pino do led como saida
+  pinMode(ledPin, OUTPUT); // Configura o pino dos LEDs como saída
 }
 
 void loop()
 {
-  int potValue = analogRead(potPin); //lê o valor do potenciometro
+  int potValue = analogRead(potPin); // Lê o valor do potenciometro
   int ledValue;
 
-  //mapeia o valor do potênciometro para a intensidade do led
-  ledValue = map(potValue, 0, 1023, 0, 255); //mapeia de 0 a 255
+  // Mapeia o valor do potênciometro para a intensidade do led
+  ledValue = map(potValue, 0, 1023, 0, 255); // Mapeia de 0 a 255
 
-  //Acende os LED com base no potenciometro
-  analogWrite(ledPin, ledValue); //Define a intensidade do LED
+  // Acende os LEDs com base no potenciômetro
+  analogWrite(ledPin, ledValue); // Define a intensidade do LED
 
-  delay(50); //Atraso para ter estabilidade
+  delay(50); // Pequeno atraso para ter estabilidade
 }
